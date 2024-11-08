5d8441226ff8d78c1e50ebd3f36a7153c0614b59
Babiscool <babiscool04@gmail.com>
2024-11-08 15:54:52 +0100

    Bouton On/Off LED2

--- TP1/main.c
+++ TP1/main.c
@@ -8,30 +8,37 @@
 #include <stdio.h>
 #include <string.h>
 
-void init_LD2(void) {
+void init_LD2(void)
+{
     /* on positionne ce qu'il faut dans les différents
        registres concernés */
-    RCC_AHB1ENR |= 0x01; // Activation du GPIOA
-    GPIOA_MODER = (GPIOA_MODER & 0xFFFFF3FF) | 0x00000400; //On mets GPIOA_MODER à 01 : General purpose output
+    RCC_AHB1ENR |= 0x01;                                   // Activation du GPIO
+    GPIOA_MODER = (GPIOA_MODER & 0xFFFFF3FF) | 0x00000400; // On mets GPIOA_MODER à 01 : General purpose output
     GPIOA_OTYPER &= ~(0x1 << 5);
     GPIOA_OSPEEDR |= 0x03 << 10;
     GPIOA_PUPDR &= 0xFFFFF3FF;
 }
 
-void init_PB(void) { /* GPIOC_MODER = ... */ }
-
+void init_PB(void)
+{
+    RCC_AHB1ENR |= 0x01 << 2;                 // Activation du GPIOA
+    GPIOC_MODER = (GPIOC_MODER & 0xF3FFFFFF); // On mets GPIOC_MODER à 00 : General purpose input
+}
 /* Ne pas optimiser cette fonction : on dépend du nombre
  * approximatif d'instructions qu'elle exécute
  */
-__attribute__((optimize("O0"))) void tempo_500ms(void) {
+__attribute__((optimize("O0"))) void tempo_500ms(void)
+{
     volatile uint32_t duree;
     /* estimation, suppose que le compilateur n'optimise pas trop... */
-    for (duree = 0; duree < 5600000; duree++) {
+    for (duree = 0; duree < 5600000; duree++)
+    {
         ;
     }
 }
 
-void init_USART(void) {
+void init_USART(void)
+{
     GPIOA_MODER = (GPIOA_MODER & 0xFFFFFF0F) | 0x000000A0;
     GPIOA_AFRL = (GPIOA_AFRL & 0xFFFF00FF) | 0x00007700;
     USART2_BRR = get_APB1CLK() / 9600;
@@ -39,25 +46,30 @@ void init_USART(void) {
     USART2_CR2 = 0;
 }
 
-void _putc(const char c) {
+void _putc(const char c)
+{
     while ((USART2_SR & 0x80) == 0)
         ;
     USART2_DR = c;
 }
 
-void _puts(const char *c) {
+void _puts(const char *c)
+{
     int len = strlen(c);
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++)
+    {
         _putc(c[i]);
     }
 }
 
-char _getc(void) {
+char _getc(void)
+{
     /* À compléter */
     return 0;
 }
 
-int main(void) {
+int main(void)
+{
     printf("\e[2J\e[1;1H\r\n");
     printf("\e[01;32m*** Welcome to Nucleo F446 ! ***\e[00m\r\n");
 
@@ -69,7 +81,18 @@ int main(void) {
     printf("APB2CLK= %9lu Hz\r\n", get_APB2CLK());
     printf("\r\n");
 
+    init_LD2();
+    init_PB();
+
     while (1)
-        ;
+    {
+
+        if (GPIOC_IDR & (1 << 13)) {
+            GPIOA_ODR &= ~(1 << 5);
+        }
+        else {
+            GPIOA_ODR |= 1 << 5;
+        }
+    }
     return 0;
 }
