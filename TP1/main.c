96792d83a28a47e73b94f785c5de173e1cfc15f8
Arthur Louvet <arthur.louvet@etu.univ-grenoble-alpes.fr>
2024-12-06 13:56:40 +0100

    tous ce qui a été fait jusqu'au 6/12

--- TP1/main.c
+++ TP1/main.c
@@ -52,14 +52,24 @@ void _puts(const char *c) {
     }
 }
 
-char _getc(void) {
-    /* À compléter */
-    return 0;
+char _getc(void)
+{
+    // Attendre que le registre de réception contienne des données
+    // Le bit 5 (RXNE) du registre de status indique qu'un caractère est disponible
+    while ((USART2_SR & 0x20) == 0)
+    {
+        // Attente active jusqu'à ce qu'un caractère soit reçu
+        // Le programme reste bloqué ici jusqu'à la réception d'un caractère
+    }
+
+    // Lire et retourner le caractère reçu
+    return (char)USART2_DR;
 }
 
-int main(void) {
+int main(void)
+{
     printf("\e[2J\e[1;1H\r\n");
-    printf("\e[01;32m*** Welcome to Nucleo F446 ! ***\e[00m\r\n");
+    printf("\e[01;32m Welcome to Nucleo F446 ! \e[00m\r\n");
 
     printf("\e[01;31m\t%08lx-%08lx-%08lx\e[00m\r\n", DESIG_UNIQUE_ID0,
            DESIG_UNIQUE_ID1, DESIG_UNIQUE_ID2);
@@ -70,6 +80,17 @@ int main(void) {
     printf("\r\n");
 
     while (1)
-        ;
+    {
+        char c = _getc();
+        if (c == 'a')
+        {
+            _putc(0x0A);
+        }
+        else
+        {
+            _putc(c);
+        }
+    }
+
     return 0;
 }
